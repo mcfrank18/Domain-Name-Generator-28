adf6c01e949ce5e574c432f3dcb6f618a2769954
mcfrank18 <fmperezg2010@gmail.com>
2022-07-30 00:06:09 +0000

    tarea entregada

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let p = 0; p < 2; p++) {
+  for (let a = 0; a < 2; a++) {
+    for (let n = 0; n < 2; n++) {
+      console.log(pronoun[p] + adj[a] + noun[n] + ".com");
+    }
+  }
+}
